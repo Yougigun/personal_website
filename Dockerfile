f7c0ef63fb35b43a26ee09bd6443dfb9d325693d
gary.y <s14031403@gmail.com>
2023-07-03 22:47:15 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Start from the latest Rust base image
+FROM rust:1.70.0 as builder
+
+# Create a new empty shell project
+WORKDIR /usr/src/app
+
+# Copy our source code
+COPY ./ ./
+
+# Build for release
+RUN cargo build --release
+
+# Start a new stage
+FROM debian:buster-slim
+
+# Copy the binary from builder to this new stage
+COPY --from=builder /usr/src/app/target/release/personal_website /usr/local/bin
+
+# Command to run the binary
+ENTRYPOINT ["personal_website"]
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[allow(unused_imports)]
+use log::{debug, error, info};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body(
+        r#"<!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            .center {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                font-size: 3em;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="center">
+            <p>Gary's website!</p>
+        </div>
+    </body>
+    </html>"#,
+    )
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let server = HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::new("%a %r %s bytes %b %D ms"))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run();
+    info!("Starting server...");
+    server.await
+}
