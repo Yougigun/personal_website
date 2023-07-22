b8ca900e5d103a932a8a272f866a7a95b3cfddac
gary.y <s14031403@gmail.com>
2023-07-22 15:29:50 +0200

    demo for bnb

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,30 @@ async fn hello() -> impl Responder {
         
                 .center {
                     display: flex;
+                    flex-direction: column;
                     justify-content: center;
                     align-items: center;
                     height: 100vh;
                     font-size: 3em;
                     color: gold; /* Change this to any shiny color you prefer */
                 }
+        
+                .center button {
+                    font-size: 1em;
+                    margin-top: 20px;
+                }
             </style>
         </head>
         <body>
             <div class="center">
                 <p>Welcome to Gary's website!!</p>
+                <a href="http://www.bnb.tw" target="_blank">
+                    <button>Visit BNB</button>
+                </a>
             </div>
         </body>
         </html>
+        
         "#,
     )
 }
@@ -37,6 +47,104 @@ async fn hello() -> impl Responder {
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+#[get("/demo/bnb")]
+async fn bnb(_req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(r#"
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {
+                background-color: #F7F7F7;
+                font-family: Arial, sans-serif;
+            }
+    
+            .container {
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+    
+            .booking {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                margin-bottom: 20px;
+            }
+    
+            .room {
+                width: 50%;
+                margin: 20px;
+                padding: 20px;
+                box-sizing: border-box;
+                border-radius: 15px;
+                background-color: white;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                text-align: center;
+            }
+    
+            .room img {
+                width: 70%;
+                border-radius: 10px;
+            }
+    
+            .room button {
+                background-color: #FF5A5F;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 10px 2px;
+                cursor: pointer;
+                border-radius: 4px;
+            }
+    
+            .booking div {
+                margin-bottom: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to Urban adobe!</h1>
+            <div class="booking">
+                <div>
+                    <label for="date">Select date:</label>
+                    <input type="date" id="date" name="date">
+                </div>
+                <div>
+                    <p id="available">Available rooms: 10</p>
+                </div>
+            </div>
+            <div class="room">
+                <img src="https://static.owlting.com/booking/image/h/4396e57d-d87f-4f13-b353-ac6fffbe0ff0/images/5HRPDjpiBb4RN8fcRzOgGKvwhnkhYTOsxmknKzA1.jpeg" alt="Room 1">
+                <h2>Room 1</h2>
+                <p>Details about the room...</p>
+                <a href="http://www.bnb.tw" target="_blank"><button>Book Now</button></a>
+            </div>
+            <!-- Add more rooms as needed -->
+        </div>
+    
+        <script>
+            // This is just a simple script to randomly update the number of available rooms
+            const available = document.getElementById('available');
+    
+            document.getElementById('date').addEventListener('change', function() {
+                const numAvailable = Math.floor(Math.random() * 11); // random number between 0 and 10
+                available.textContent = `Available rooms: ${numAvailable}`;
+            });
+        </script>
+    </body>
+    </html>
+    "#
+    
+    ,)
 }
 
 async fn manual_hello() -> impl Responder {
@@ -52,6 +160,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::new("%a %r %s %b(bytes) %D(ms)"))
             .service(hello)
             .service(echo)
+            .service(bnb)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
